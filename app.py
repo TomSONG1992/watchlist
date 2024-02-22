010e7b4ec258603a4c3a1ff8cef29cbe4211cd67
songdexiongi <211125960@qq.com>
2024-02-22 10:04:30 +0800

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,29 @@
+# File created by: songdexiong
+# Created on: 2024/2/22
+# Last modified on: 9:14
+# Description:
+from flask import url_for
+from flask import  Flask
+from markupsafe import escape
+
+app= Flask(__name__)
+@app.route('/')
+@app.route('/home')
+@app.route('/index')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
