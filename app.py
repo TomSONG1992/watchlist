eadbe58daf92ef7c633412ab303016f73b55fc0a
songdexiongi <211125960@qq.com>
2024-02-22 15:32:50 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -4,12 +4,20 @@
 # Description:
 from flask import url_for
 from flask import  Flask,render_template
-
+from flask import _app_ctx_stack
 from markupsafe import escape
-
+import os
+import sys
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pymysql
 
 app= Flask(__name__)
-app.run(debug=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sihoo%402022@127.0.0.1/watchlist'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -49,3 +57,48 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+import click
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
