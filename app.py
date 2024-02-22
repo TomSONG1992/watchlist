59fd69fcce85b8821db74047b42586c3b552a3f9
songdexiongi <211125960@qq.com>
2024-02-22 16:18:38 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -40,7 +40,10 @@ movies = [
 
 @app.route('/index')
 def index():
-    return render_template('index.html',name= name,movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+
 
 
 @app.route('/user/<name>')
@@ -73,32 +76,41 @@ class Movie(db.Model):  # 表名将会是 movie
 import click
 
 
-@app.cli.command()
-def forge():
-    """Generate fake data."""
-    db.create_all()
+# @app.cli.command()
+# def forge():
+#     """Generate fake data."""
+#     db.create_all()
+#
+#     # 全局的两个变量移动到这个函数内
+#     name = 'Grey Li'
+#     movies = [
+#         {'title': 'My Neighbor Totoro', 'year': '1988'},
+#         {'title': 'Dead Poets Society', 'year': '1989'},
+#         {'title': 'A Perfect World', 'year': '1993'},
+#         {'title': 'Leon', 'year': '1994'},
+#         {'title': 'Mahjong', 'year': '1996'},
+#         {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#         {'title': 'King of Comedy', 'year': '1999'},
+#         {'title': 'Devils on the Doorstep', 'year': '1999'},
+#         {'title': 'WALL-E', 'year': '2008'},
+#         {'title': 'The Pork of Music', 'year': '2012'},
+#     ]
+#
+#     user = User(name=name)
+#     db.session.add(user)
+#     for m in movies:
+#         movie = Movie(title=m['title'], year=m['year'])
+#         db.session.add(movie)
+#
+#     db.session.commit()
+#     click.echo('Done.')
 
-    # 全局的两个变量移动到这个函数内
-    name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
-    user = User(name=name)
-    db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
 
-    db.session.commit()
-    click.echo('Done.')
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
